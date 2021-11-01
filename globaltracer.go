efc55d8dcc8f4c125b456c02e03d61907d24007c
Kieran Hu Cang <kieran.hucang@shopee.com>
2021-11-01 18:05:16 +0800

    doc(globaltracer) add globaltracer doc

--- globaltracer.go
+++ globaltracer.go
@@ -9,33 +9,32 @@ var (
 	globalTracer = registeredTracer{NoopTracer{}, false}
 )
 
-// SetGlobalTracer sets the [singleton] opentracing.Tracer returned by
-// GlobalTracer(). Those who use GlobalTracer (rather than directly manage an
-// opentracing.Tracer instance) should call SetGlobalTracer as early as
-// possible in main(), prior to calling the `StartSpan` global func below.
-// Prior to calling `SetGlobalTracer`, any Spans started via the `StartSpan`
-// (etc) globals are noops.
+// SetGlobalTracer 设置一个[单例]的追踪系统。 Tracer 可以使用 GlobalTracer() 返回。
+// 不管谁使用 GlobalTracer（而不是指直接管理 opentracing.Tracer 的实例），
+// 都应该在main()中尽早的调用 SetGlobalTracer，应在`StartSpan`的调用之前。
+// 在调用`SetGlobalTracer`之前，任何通过`StartSpan`创建的Span都是来自noop的。
 func SetGlobalTracer(tracer Tracer) {
 	globalTracer = registeredTracer{tracer, true}
 }
 
-// GlobalTracer returns the global singleton `Tracer` implementation.
-// Before `SetGlobalTracer()` is called, the `GlobalTracer()` is a noop
-// implementation that drops all data handed to it.
+// GloablTracer 返回`Tracer`实现的全局单例。
+// 在调用`SetGlobalTracer()`之前，`GlobalTracer()`返回的是noop实现，它会丢掉所有的数据。
 func GlobalTracer() Tracer {
 	return globalTracer.tracer
 }
 
-// StartSpan defers to `Tracer.StartSpan`. See `GlobalTracer()`.
+// StartSpan 遵从 Tracer.StartSpan，见 `GlobalTracer()`。
 func StartSpan(operationName string, opts ...StartSpanOption) Span {
 	return globalTracer.tracer.StartSpan(operationName, opts...)
 }
 
+// InitGlobalTracer 已废弃(deprecated)，请使用 SetGlobalTracer。
 // InitGlobalTracer is deprecated. Please use SetGlobalTracer.
 func InitGlobalTracer(tracer Tracer) {
 	SetGlobalTracer(tracer)
 }
 
+// IsGlobalTracerRegistered 返回一个布尔值去判断tracer是否已经在全局注册
 // IsGlobalTracerRegistered returns a `bool` to indicate if a tracer has been globally registered
 func IsGlobalTracerRegistered() bool {
 	return globalTracer.isRegistered
